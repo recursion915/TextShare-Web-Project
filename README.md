0b9581a933841361e7182a0f7a7fe9636a06674b
CZ_WY <Titan@dhcp-10-5-1-189.wireless.rochester.edu>
2015-11-06 15:07:15 -0500

    add all

---
+++ README.md
@@ -0,0 +1,44 @@
+# csc210project
+Chen Zhang Yue Wang
+CSC210 Project for Fall-2015
+Chen Zhang: czhang29@ur.rochester.edu
+Yue Wang: yuewang0319@gmail.com
+
+
+
+
+
+Who is the target audience for your Web application?
+
+[Optional stretch goal: Identify a target audience that none of your group members belong to. This will make your project more unique than a Web app for college students.]
+
+Our target audiences are mainly college students, who seek to use textbooks and textbooks owners. 
+
+What problem is it intended to solve for that audience?
+
+
+
+How will it meet the minimum project requirements?
+
+Users can create accounts to become both textbook provider and receiver, and post textbook info on a webpage
+Users can search for the textbooks they intend to rent and search results will be presented on a webpage 
+Users can update their existing posts
+Users can delete their posts and accounts
+
+
+Why is your proposed Web application unique or creative beyond simply meeting the minimum requirements?
+
+Current
+
+Disadvantages
+
+
+
+Features 
+
+Purpose
+
+Our textbook rental website applies the ideas of "shareing economy" and "P2P rental market". Our purpose is to make it easier for textbook owners and those who seek 
+to use textbooks find each other. On one hand, it allows book owners to make money from underused textbooks. On the other hand, it enables whoever seek to use 
+textbooks to rent them with cheaper prices.
+
---
+++ server.js
@@ -0,0 +1,113 @@
+
+var express = require('express');
+var app = express();
+var db=require('./db');
+var fs = require('fs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static('public_files'));
+
+app.post('/users',function(req,res){
+    var postBody=req.body;
+    var myUsername=postBody.username;
+//         console.log(myUsername);
+    if(!myUsername){
+        res.send('ERROR');
+        return;
+    }
+    
+    function getQuery(username, callback){
+    db.query('SELECT username FROM users WHERE username=?', username,function(err,results,rows){
+                                    if(err){
+                                    callback(err,null);}
+                                    else{
+                                    callback(null,results[0]);}
+             });
+         }
+
+    var existUsername=getQuery(myUsername,function(err,data){
+                if(err){
+                    throw err;}
+           
+                else{
+//                console.log(data.username);
+                if(!data){
+                        var user={username:postBody.username,password:postBody.password,firstname:postBody.firstname,lastname:postBody.lastname,address:postBody.address,university:postBody.university};
+                        var query=db.query('INSERT INTO users SET ?', user, function(err,res){
+                                if(err){throw err;}
+                                console.log('Last insert ID:',res.insertId);
+                        });
+                        res.send('OK');
+                        return;
+                               }
+                else{
+                               res.send('EXIST');
+                return;}
+
+                }
+        });
+    });
+
+app.post('/users2/',function(req,res){
+         var postBody=req.body;
+         var nameToLookup=postBody.username;
+         var passwordToCheck=postBody.password;
+//          console.log(nameToLookup);
+//         console.log(passwordToCheck);
+        
+    if(!nameToLookup){
+         res.send('ERROR');
+         //send empty json object
+         res.send('{}')
+         return;
+    }
+    
+//         function getQuery(nameToLookup,callback){
+         db.query('SELECT * FROM users WHERE username=?', nameToLookup,function(err,rows){
+                  if(err)throw err;
+                  else{
+                  var jsonObject=rows[0];
+                  console.log(jsonObject);
+                  console.log('password entered is '+passwordToCheck+'orignial password is '+jsonObject.password);
+                  if(passwordToCheck===jsonObject.password)
+                  {
+                  res.send(jsonObject);
+                  }
+                  else
+                  {
+                  //send empty json object if the password is wrong
+                  res.send('{}');
+                  }
+                  }
+            });
+//         }
+//         console.log(results[0]);
+//         console.log(results[0].username);
+//         console.log(results[0].password);
+//         var abc=getQuery(nameToLookup,function(err,data){
+//                                    if(err){
+//                                    throw err;}
+//                                    
+//                                    else{
+//                          console.log(data.username);
+//                          console.log(data.password);
+//                          }
+//                          });
+         
+//    if(passwordToCheck==dbPassword){
+//         res.send('OK');
+//         return;
+//    }
+//    
+//    res.send('UNMATCH');
+//    
+   });
+
+var server = app.listen(3000, function () {
+    var port = server.address().port;
+    console.log('Server started at http://localhost:%s/', port);
+});
